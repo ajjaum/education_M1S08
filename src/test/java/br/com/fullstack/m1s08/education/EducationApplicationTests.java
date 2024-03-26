b075f00b3165ad6674fc70446dbd7b5708f221c4
ajjaum <j061701@dac.unicamp.br>
2024-03-25 23:07:43 -0300

    Exercício 1 - M1S08.

---
+++ src/test/java/br/com/fullstack/m1s08/education/EducationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.m1s08.education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
