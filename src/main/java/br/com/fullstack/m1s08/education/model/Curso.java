127f3e1fb48bbb5652e90c5d78c80e6cc304c26f
ajjaum <j061701@dac.unicamp.br>
2024-03-26 23:33:53 -0300

    Exercício 5 - M1S08.
    Criação Service Curso

--- src/main/java/br/com/fullstack/m1s08/education/model/Curso.java
+++ src/main/java/br/com/fullstack/m1s08/education/model/Curso.java
@@ -10,31 +10,23 @@ import java.util.List;
 public class Curso {
 
     private static Integer proximoId = 1;
-    @Getter
-    private static List<Curso> cursosCadastrados = new ArrayList<>();
+    @Getter private static final List<Curso> cursosCadastrados = new ArrayList<>();
 
     private Integer id;
     @Setter private String nome;
     @Setter private String descricao;
     @Setter private Integer cargaHoraria;
+    private List<Aluno> alunos = new ArrayList<>();
 
-    public static Curso salvar(Curso curso) throws Exception{
-        if (validar(curso)) {
-            curso.id = proximoId++;
-            cursosCadastrados.add(curso);
-        }
+    public static Curso inserir(Curso curso) throws Exception {
+        curso.id = proximoId++;
+        cursosCadastrados.add(curso);
         return curso;
     }
 
-    public static Curso salvar(Integer id, Curso curso) throws Exception{
-        if (validar(curso)) {
-            Curso cadastrado = buscarPorId(id);
-            cadastrado.setNome(curso.getNome());
-            cadastrado.setDescricao(curso.getDescricao());
-            cadastrado.setCargaHoraria(curso.getCargaHoraria());
-            return cadastrado;
-        }
-        return null;
+    public static boolean excluir(Curso curso) throws Exception {
+        cursosCadastrados.remove(curso);
+        return true;
     }
 
     public static Curso buscarPorId(Integer id) throws Exception {
@@ -46,13 +38,7 @@ public class Curso {
         throw new Exception("Curso não encontrado");
     }
 
-    private static boolean validar(Curso curso) throws Exception{
-        if (curso.getNome() == null || curso.getNome().isEmpty()) {
-            throw new Exception("Nome é obrigatório");
-        }
-        if (curso.getCargaHoraria() == null || curso.getCargaHoraria() < 100) {
-            throw new Exception("Carga horária não informada ou menor do que 100 horas.");
-        }
-        return true;
+    public static void adicionarAluno(Curso curso, Aluno aluno) throws Exception {
+        curso.getAlunos().add(aluno);
     }
 }
